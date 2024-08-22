a03eb4e14bab9c4b48f270304338b58c78b5cb96
Cynthia Muyunda <muyundacynthia@gmail.com>
2024-08-22 11:36:21 +0200

    fix footer gap issue

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-import { public_key, template_id, service_id } from "./config.js";
+
 
 //Get the button
 let mybutton = document.getElementById("btn-back-to-top");
@@ -26,37 +26,18 @@ function backToTop() {
   document.documentElement.scrollTop = 0;
 }
 
-// sending email using EmailJS
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();  // Prevent the default form submission
 
-(function(){
-    emailjs.init(PUBLIC_KEY); // Replace with your EmailJS user ID
- })();
-
- document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent the default form submission
- 
-    // Collect the form data
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
     const subject = document.getElementById('subject').value;
     const message = document.getElementById('message').value;
- 
-    // Define the email parameters
-    const templateParams = {
-       from_name: name,
-       from_email: email,
-       subject: subject,
-       message: message
-    };
- 
-    // Send the email using EmailJS
-    emailjs.send(SERVICE_ID, TEMPLATE_ID, templateParams)
-    .then(function(response) {
-       console.log('SUCCESS!', response.status, response.text);
-       alert('Your message has been sent!');
-    }, function(error) {
-       console.log('FAILED...', error);
-       alert('Sorry, there was an error sending your message. Please try again later.');
-    });
- });
+
+    // Construct the mailto link
+    const mailtoLink = `mailto:cynmuy@gmail.com?subject=${encodeURIComponent(subject)}&body=Name: ${encodeURIComponent(name)}%0AEmail: ${encodeURIComponent(email)}%0A%0A${encodeURIComponent(message)}`;
+
+    // Open the default mail client with the mailto link
+    window.location.href = mailtoLink;
+});
  
